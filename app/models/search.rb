20d355a937ec82de4e1b28aa8465a73a7abeb2ee
Matt Stempeck <stempeck@gmail.com>
2012-05-10 16:38:47 -0400

    searches, schema

--- app/models/search.rb
+++ app/models/search.rb
@@ -6,7 +6,7 @@ class Search < ActiveRecord::Base
   end
   
   def been_run?
-    self.last_id != nil && self.last_id != nil
+    self.last_id != nil && self.last_id != ""
   end
 
   def update_results
@@ -29,14 +29,12 @@ class Search < ActiveRecord::Base
   end
   
   private 
-    
     def latest_results
-      # grab latest results
-      # if search run before, only grab new results
-      if been_run? 
-        results = Twitter.search(query, :since_id=>self.last_id, :rpp=>100, :include_entities=>true)
-      else 
-        results = Twitter.search(query, :rpp=>100, :include_entities=>true)
+      results = []
+      if self.category == "Search"
+        results = latest_search_results 
+      elsif self.category == "List"
+        results = latest_list_results
       end
       # update last ID in model and only update last ID if results were received
       logger.info "Ran Twitter search for" + query
@@ -45,26 +43,56 @@ class Search < ActiveRecord::Base
       return results
     end
     
+    def latest_list_results
+      # split query into username and listname
+      query_parts = self.query.split('/')
+      username = query_parts.first
+      listname = query_parts.second
+      # get new results from list
+      if been_run? 
+        results = Twitter.list_timeline(username, listname, :per_page=>100, :since_id=>self.last_id, :include_entities=>true)
+      else 
+        results = Twitter.list_timeline(username, listname, :per_page=>100, :include_entities=>true)
+      end
+      return results
+    end
+    
+    def latest_search_results
+      # grab latest results from a search
+      # if search run before, only grab new results
+      if been_run? 
+        results = Twitter.search(query, :since_id=>self.last_id, :rpp=>100, :include_entities=>true)
+      else 
+        results = Twitter.search(query, :rpp=>100, :include_entities=>true)
+      end
+      return results
+    end
+    
     def save_tweeted_links tweets
       logger.info "Trying to find links in " + tweets.count.to_s
       saved_links = 0
       tweets.each do |tweet|
         links = tweet.expanded_urls
+        # this works if it's a Search
+        user = tweet.from_user
+        # this works if it's a List
+        user = tweet.user.screen_name if user == nil
         if links != nil
           links.each do |link_url|
-            existing_link = Link.find_by_url(link_url)
+            cleaned_link_url = link_url.downcase
+            existing_link = Link.find_by_url(cleaned_link_url)
             if existing_link == nil
               new_link = Link.new
-              new_link.url = link_url
+              new_link.url = cleaned_link_url
               new_link.frequency = 1
-              new_link.add_source(tweet.from_user)
+              new_link.add_source(user)
               new_link.search_id = id
               new_link.first_tweeted = tweet.created_at
               new_link.moderation_status = :new
               new_link.save
             else
               existing_link.frequency = existing_link.frequency + 1
-              existing_link.add_source(tweet.from_user)
+              existing_link.add_source(user)
               existing_link.search_id = id
               existing_link.save
             end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120423183721) do
+ActiveRecord::Schema.define(:version => 20120508181546) do
 
   create_table "links", :force => true do |t|
     t.integer  "search_id"
@@ -27,10 +27,11 @@ ActiveRecord::Schema.define(:version => 20120423183721) do
   create_table "searches", :force => true do |t|
     t.string   "query"
     t.string   "last_id"
-    t.datetime "created_at",                          :null => false
-    t.datetime "updated_at",                          :null => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.integer  "featured_link_id"
     t.boolean  "active",           :default => false
+    t.string   "category",         :default => "Search"
   end
 
 end
